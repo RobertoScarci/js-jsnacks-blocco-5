145a6e50abe544e17f869724b5b172fd5d154cc2
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-27 11:35:58 +0100

    creazione della lista di veicoli

---
+++ snack-1/js/script.js
@@ -0,0 +1,53 @@
+// creo la lista delle macchine
+const carList = [
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'BMW',
+        modello: 'serie 1',
+        alimentazione: 'diesel'
+    }
+]
