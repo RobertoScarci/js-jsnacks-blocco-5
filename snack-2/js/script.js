6cd1e61bc7add6667495cf4baa16c80b0207da2c
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-27 12:17:10 +0100

    creazione dell'array

---
+++ snack-2/js/script.js
@@ -0,0 +1,38 @@
+// creo una lista di persone
+const list = [
+    {
+        name: 'Anakin',
+        surname: 'Skywalker',
+        age: '22'
+    },
+    {
+        name: 'Greta',
+        surname: 'Thumberg',
+        age: '16'
+    },
+    {
+        name: 'Walter',
+        surname: 'White',
+        age: '54'
+    },
+    {
+        name: 'Alessia',
+        surname: 'Porcino',
+        age: '19'
+    },
+    {
+        name: 'Obi-Wan',
+        surname: 'Kenobi',
+        age: '42'
+    },
+    {
+        name: 'Francesco',
+        surname: 'Camarda',
+        age: '15'
+    },
+    {
+        name: 'Jude',
+        surname: 'Bellingham',
+        age: '20'
+    }
+]
